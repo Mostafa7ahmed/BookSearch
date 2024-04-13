3ab385c5968c9fba9c399ef59917abe05cc0c33b
mostafa hamed <154756540+Mostafa7ahmed@users.noreply.github.com>
2024-04-13 23:38:23 +0200

    Update ConText.js

--- src/ConText.js
+++ src/ConText.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useCallback } from 'react';
-const URL = "http://openlibrary.org/search.json?title=";
+const URL = "https://openlibrary.org/search.json?title=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
